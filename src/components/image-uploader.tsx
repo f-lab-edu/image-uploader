17f4b234fabbd274d65f3ad9bf41a904c5534554
Seung Hyun Tark <makepin2r@gmail.com>
2025-08-01 00:43:11 +0900

    update: 이미지 업로드 UI 드래그 이펙트 추가

--- src/components/image-uploader.tsx
+++ src/components/image-uploader.tsx
@@ -1,8 +1,48 @@
+import React, { useState } from "react";
+
 const ImageUploader = () => {
+  const [isHover, setIsHover] = useState<boolean>(false);
+  const [isDragOver, setIsDragOver] = useState<boolean>(false);
+  const isActive = isHover || isDragOver;
+
+  const handleDrag = (
+    e: React.DragEvent<HTMLLabelElement>,
+    status: boolean
+  ) => {
+    e.preventDefault();
+    setIsDragOver(status);
+  };
+
+  const handleHover = (
+    e: React.MouseEvent<HTMLLabelElement>,
+    status: boolean
+  ) => {
+    e.preventDefault();
+    setIsHover(status);
+  };
+
   return (
     <label
       htmlFor="image-upload"
-      className="p-16 bg-indigo-50 rounded-md w-full h-64 cursor-pointer hover:bg-indigo-100 transition-colors duration-300 ease-in-out flex flex-col justify-center items-center gap-1"
+      className={`p-16 bg-gray-100 rounded-md w-full h-64 cursor-pointer transition-colors duration-300 ease-in-out flex flex-col justify-center items-center gap-1 ${
+        isActive && "bg-indigo-200"
+      }`}
+      onMouseEnter={(e) => {
+        handleHover(e, true);
+      }}
+      onMouseLeave={(e) => {
+        handleHover(e, false);
+      }}
+      onDragOver={(e) => {
+        handleDrag(e, true);
+      }}
+      onDragLeave={(e) => {
+        handleDrag(e, false);
+      }}
+      onDrop={(e) => {
+        handleDrag(e, false);
+        // TODO File Upload
+      }}
     >
       <input id="image-upload" type="file" accept="image/*" hidden multiple />
       <p className="text-lg">이미지 업로드하기</p>
