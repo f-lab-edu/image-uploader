d625b38114a717c3f4c98030c646d7b3ab0685d6
Seung Hyun Tark <makepin2r@gmail.com>
2025-08-02 11:02:49 +0900

    chore: App.tsx 업데이트 - 실제 사용 예시 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import "./App.css";
+import { useState } from "react";
+import ImageUploader from "./components/image-uploader";
+import UploadedImageList from "./components/uploaded-image-list";
+import ImagePreview from "./components/image-preview";
 
 function App() {
-  return <div>App</div>;
+  const [previewImg, setPreviewImg] = useState<File | null>(null);
+  const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+
+  const handleSelectPreviewImage = (value: File | null) => {
+    setPreviewImg(value);
+  };
+
+  const handleUploadImages = (images: File[]) => {
+    setUploadedFiles([...uploadedFiles, ...images]);
+  };
+
+  return (
+    <div className="w-full h-full p-20 grid grid-cols-2 gap-4">
+      <div className="flex flex-col gap-4">
+        <ImageUploader
+          onUploadImages={handleUploadImages}
+          acceptedExt={["jpg", "jpeg"]}
+        />
+        <UploadedImageList
+          images={uploadedFiles}
+          onSelectImage={handleSelectPreviewImage}
+        />
+      </div>
+      <ImagePreview image={previewImg} />
+    </div>
+  );
 }
 
 export default App;
