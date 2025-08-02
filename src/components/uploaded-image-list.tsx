0295e2545c16436e36dd4d8179d2b8f7b8da0fd9
Seung Hyun Tark <makepin2r@gmail.com>
2025-08-03 02:08:17 +0900

    update: 업로드 이미지 UI 수정 - 썸네일, 업로드 일시, 용량 정보 추가 - 데이터 타입 File -> FileInfo로 변경

--- src/components/uploaded-image-list.tsx
+++ src/components/uploaded-image-list.tsx
@@ -1,5 +1,8 @@
+import type { FileInfo } from "../types/file-types";
+import DeleteButton from "./ui/delete-button";
+
 interface UploadedImageListProps {
-  images: File[];
+  images: FileInfo[];
   onSelectImage: (idx: number) => void;
   onDeleteImage: (idx: number) => void;
 }
@@ -11,14 +14,33 @@ const UploadedImageList = ({
 }: UploadedImageListProps) => {
   return (
     <div>
-      <ul className="flex gap-2 flex-wrap">
+      <ul className="flex flex-col gap-2">
         {images.map((image, i) => (
           <li
             key={`image-${i}-${image.name}`}
-            className="border-2 border-indigo-200 rounded-full px-2 py-1 hover:bg-indigo-200 transition-colors duration-300 ease-in-out flex gap-2"
+            className="border-2 border-indigo-200 rounded-md hover:border-indigo-600 transition-colors duration-300 ease-in-out flex justify-start h-20 overflow-hidden"
           >
-            <button onClick={() => onSelectImage(i)}>{image.name}</button>
-            <button onClick={() => onDeleteImage(i)}>X</button>
+            <button
+              onClick={() => onSelectImage(i)}
+              className="w-[5rem] aspect-square"
+            >
+              <img src={image.url} className="w-full h-full object-cover" />
+            </button>
+            <div className="w-[calc(100%-5rem)] h-full shrink-0 p-4 flex gap-2 justify-between items-center">
+              <button
+                onClick={() => onSelectImage(i)}
+                className="flex flex-col gap-1 justify-center items-start w-[calc(100%-28px)]"
+              >
+                <p className="text-md font-semibold w-full overflow-hidden text-ellipsis text-left">
+                  {image.name}
+                </p>
+                <p className="text-gray-500 text-sm">
+                  {image.createdAt.toLocaleString()},{" "}
+                  {Math.floor((image.size / (1024 * 1024)) * 100) / 100} MB
+                </p>
+              </button>
+              <DeleteButton onClick={() => onDeleteImage(i)} />
+            </div>
           </li>
         ))}
       </ul>
