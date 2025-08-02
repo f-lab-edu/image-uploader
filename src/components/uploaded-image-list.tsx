059f588f0fe1769916ad05a39d84217eef37925c
Seung Hyun Tark <makepin2r@gmail.com>
2025-08-02 18:44:23 +0900

    update: 업로드된 이미지 리스트 UI 삭제 기능 추가 - 미리보기 선택 함수 인자 File -> 인덱스로 변경 - 삭제 함수 props 추가

--- src/components/uploaded-image-list.tsx
+++ src/components/uploaded-image-list.tsx
@@ -1,23 +1,24 @@
 interface UploadedImageListProps {
   images: File[];
-  onSelectImage: (img: File) => void;
+  onSelectImage: (idx: number) => void;
+  onDeleteImage: (idx: number) => void;
 }
 
 const UploadedImageList = ({
   images,
   onSelectImage,
+  onDeleteImage,
 }: UploadedImageListProps) => {
   return (
     <div>
       <ul className="flex gap-2 flex-wrap">
         {images.map((image, i) => (
-          <li key={`image-${i}-${image.name}`}>
-            <button
-              onClick={() => onSelectImage(image)}
-              className="border-2 border-indigo-200 rounded-full px-2 py-1 hover:bg-indigo-200 transition-colors duration-300 ease-in-out"
-            >
-              {image.name}
-            </button>
+          <li
+            key={`image-${i}-${image.name}`}
+            className="border-2 border-indigo-200 rounded-full px-2 py-1 hover:bg-indigo-200 transition-colors duration-300 ease-in-out flex gap-2"
+          >
+            <button onClick={() => onSelectImage(i)}>{image.name}</button>
+            <button onClick={() => onDeleteImage(i)}>X</button>
           </li>
         ))}
       </ul>
