3db2d6f95d8a4778f24d4e7915a16b350123989c
Seung Hyun Tark <makepin2r@gmail.com>
2025-08-02 20:58:54 +0900

    feat: 버튼 공통 컴포넌트 정의

---
+++ src/components/common/button.tsx
@@ -0,0 +1,22 @@
+const BUTTON_SIZE = {
+  sm: "28",
+  md: "36",
+};
+
+type ButtonSizeType = keyof typeof BUTTON_SIZE;
+
+export interface ButtonProps {
+  onClick: () => void;
+  size?: ButtonSizeType;
+  icon: React.ReactElement;
+}
+
+const Button = ({ onClick, size = "md", icon }: ButtonProps) => {
+  return (
+    <button onClick={onClick} className={`hover:text-indigo-600`}>
+      <icon.type width={BUTTON_SIZE[size]} height={BUTTON_SIZE[size]} />
+    </button>
+  );
+};
+
+export default Button;
