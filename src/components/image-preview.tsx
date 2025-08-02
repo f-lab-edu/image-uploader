d733bedf1796e31d1a09499ea8708b7409ca57b5
Seung Hyun Tark <makepin2r@gmail.com>
2025-08-02 10:42:41 +0900

    update: 이미지 미리보기 UI 컴포넌트 예외 처리 추가 - image props 타입 null 포함 - image props 변경 시 image가 null일 경우 예외 처리 추가 - image null일 경우 placeholder 추가

--- src/components/image-preview.tsx
+++ src/components/image-preview.tsx
@@ -1,33 +1,45 @@
 import { useEffect, useState } from "react";
 interface ImagePreviewProps {
-  image: File;
+  image: File | null;
 }
 
 const ImagePreview = ({ image }: ImagePreviewProps) => {
   const [previewImg, setPreviewImg] = useState<string>("");
 
   useEffect(() => {
-    const reader = new FileReader();
-    reader.readAsDataURL(image);
-    reader.onloadend = () => {
-      if (typeof reader.result === "string") {
-        setPreviewImg(reader.result);
-      } else {
-        alert("이미지를 읽는 데 실패했습니다.");
-        console.warn("unexpected type from FileReader:", reader.result);
-        setPreviewImg("");
-      }
-    };
+    if (image === null) {
+      setPreviewImg("");
+    } else {
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onloadend = () => {
+        if (typeof reader.result === "string") {
+          setPreviewImg(reader.result);
+        } else {
+          alert("이미지를 읽는 데 실패했습니다.");
+          console.warn("unexpected type from FileReader:", reader.result);
+          setPreviewImg("");
+        }
+      };
+    }
   }, [image]);
 
   return (
     <div className="w-full">
-      <img
-        src={previewImg ? previewImg : "/img_dummy.png"}
-        alt={image.name}
-        className="w-full aspect-[4/3] object-cover"
-      />
-      <p>{image.name}</p>
+      {image ? (
+        <>
+          <img
+            src={previewImg ? previewImg : "/img_dummy.png"}
+            alt={image.name}
+            className="w-full aspect-[4/3] object-cover"
+          />
+          <p>{image.name}</p>
+        </>
+      ) : (
+        <div className="w-full aspect-[4/3] bg-gray-200 flex justify-center items-center">
+          <p>미리보기 이미지를 선택해주세요.</p>
+        </div>
+      )}
     </div>
   );
 };
