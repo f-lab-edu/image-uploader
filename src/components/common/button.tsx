51cf952bd2f8ebe3679f185a126fd52c3af4b51c
Seung Hyun Tark <makepin2r@gmail.com>
2025-08-03 02:17:32 +0900

    style: 버튼 공통 컴포넌트 스타일 추가 - 트랜지션

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -13,7 +13,10 @@ export interface ButtonProps {
 
 const Button = ({ onClick, size = "sm", icon }: ButtonProps) => {
   return (
-    <button onClick={onClick} className={`text-gray-400 hover:text-gray-600`}>
+    <button
+      onClick={onClick}
+      className={`text-gray-400 hover:text-gray-600 transition-colors duration-300 ease-in-out`}
+    >
       <icon.type width={BUTTON_SIZE[size]} height={BUTTON_SIZE[size]} />
     </button>
   );
